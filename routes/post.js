6c34e69b47a11bae795da2989de76099797b660f
qlido <jason7530@naver.com>
2022-10-23 03:30:48 +0900

    add : BOARD 불러오기 쓰기 업데이트 지우기

---
+++ routes/post.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const models = require('../database/models');
+const { isLoggedIn, isNotLoggedIn } = require('./isLogined');
+
+router.get('/',async (req, res, next) => {
+    models.Board.findAll({
+        include: [
+            {
+                model: models.Users,
+                attributes: ['code', 'name', 'nickname'],
+            }
+        ],
+        where : {allowBoard : true},
+        order: [['boardCode', 'ASC']],
+    }).then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        console.error(err);
+        res.status(500).send('Server Error');
+    })
+})
+
+router.post('/',isLoggedIn,async (req, res, next) => {
+    //console.log(req);
+    models.Board.create({
+        title : req.body.title,
+        contents : req.body.contents,
+        allowBoard : req.body.allowBoard,
+        Usercode: req.user.code
+    }).then(() => {
+        return  res.status(200).send('Success');
+    }).catch((err) => {
+        //console.error(err);
+        return res.status(500).send('Server Error');
+    })
+})
+
+router.delete('/',isLoggedIn,async (req, res, next) => {
+    models.Board.destroy({
+        where: {boardCode: req.body.boardCode}
+    }).then(() => {
+        return res.status(200).send('Success');
+    }).catch((err) => {
+        console.error(err);
+        return res.status(500).send('Server Error');
+    })
+})
+
+router.post('/update',isLoggedIn,async (req, res, next) => {
+    models.Board.update({
+        allowBoard : req.body.allowBoard,
+    },{
+        where: {boardCode: req.body.boardCode}
+    }).then(() => {
+        return res.status(200).send('Success');
+    }).catch((err) => {
+        console.error(err);
+        return res.status(500).send('Server Error');
+    })
+})
+
+
+module.exports = router;
