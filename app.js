888f0b8e91e2b0dc697aa4e1eeada29747f07f99
qlido <jason7530@naver.com>
2022-10-28 22:45:19 +0900

    FIX: DB create moon

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ passportConfig()
 
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-models.sequelize.sync({ force: true })
+models.sequelize.sync({ force: false })
     .then(() => {
         //console.log('데이터베이스 연결 성공');
     })
