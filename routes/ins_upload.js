83c913293feb8e08dba05b3dfcdd0684fc992308
chaewoo1002 <chaewoo1002@gmail.com>
2022-11-05 22:32:55 +0900

    FIX : insta_upload.js 기능 수정

--- routes/ins_upload.js
+++ routes/ins_upload.js
@@ -1,32 +1,30 @@
-import axios from "axios";
+const axios = require('axios');
 
 const mediaUrl = "https://graph.facebook.com/v15.0/17841455266680504/media"
 const publishUrl = "https://graph.facebook.com/v15.0/17841455266680504/media_publish"
 const logoImage = "https://ifh.cc/g/Vt7g4L.png"
-const token = "EAAVNoRY1CQcBADiTB2uPky3F4ZAwQfcsLMdPViWvuQEZB9eiW4ZBAhFWT3Ecqp4NXrjdKp6AKFmYDyj6rlZBwvaXo5U3dF3IuuqN7arqACYqy9kuM2Q8duuECo4ORTuaLSU7Bq67SjxJiHnoUF2S00XzJMsgqVWlRW41r6uksBrYCKC9gVZBa"
+const token = "EAAVNoRY1CQcBAKNU3NlTLRvZAVWH0DPXraX83ZBdPONVUIIYWiTQBJidFZALyM2AZCofj2t9Bh4sQeOr6IFolWOZBGZBxkwpZCuM1oIGXxECV7qjzAh75LaZCslZCRiHWBHzfCfN7p7YKCz0EIpw2MoTqVSzksucdVcwNTJ6CKjOfChNlLBnEV5JTY1cht8ZCXlmIx243hFCrMZBLZAAbjL1XAnc7oZC9ZBb28IGZAHZCQudZAIm9FUMbatsX6Ls48KfBO4yZBTI0ZD"
 
-let containerCreate = `${mediaUrl}
-    ?image_url=${logoImage}
-    &caption=test
-    &access_token=${token}`
 
 let publishContent = `${publishUrl}
     ?creation_id={}
     &access_token=${token}`
 
 const containerCreate = {
-    method: 'post',
-    url: 'https://graph.facebook.com/v15.0/17841455266680504/media',
-        image_url: 'https://ifh.cc/g/Vt7g4L.png',
-        caption: 'test',
-        access_token: token`
-    headers: { }
+    image_url: 'https://ifh.cc/g/Vt7g4L.png',
+    caption: 'test',
+    access_token: token
 };
+let mediaId = null;
+let mdurl = mediaUrl + "?image_url=" + containerCreate.image_url + "&caption=" + containerCreate.caption + "&access_token=" + containerCreate.access_token;
 
-axios(containerCreate)
-    .then(function (response) {
-        console.log(JSON.stringify(response.data));
+
+axios.post(mdurl)
+    .then( (response) => {
+        mediaId = response.data.id;
+    }).then(()=>{
+    let pburl = publishUrl + "?creation_id=" + mediaId + "&access_token=" + containerCreate.access_token;
+    axios.post(pburl).then((response) => {
+        console.log(`success`);
     })
-    .catch(function (error) {
-        console.log(error);
-    });
+})
