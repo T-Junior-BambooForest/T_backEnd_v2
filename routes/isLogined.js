9210ce47f87b7a961f36e553843c8a6e9f1bbbac
qlido <jason7530@naver.com>
2022-11-16 16:13:07 +0900

    UPDATE: console.log정리

--- routes/isLogined.js
+++ routes/isLogined.js
@@ -16,6 +16,9 @@ exports.auth = (req, res, next) => {
         // 요청 헤더에 저장된 토큰(req.headers.authorization)과 비밀키를 사용하여 토큰을 req.decoded에 반환
         //console.log(req.cookies);
         //req.decoded = jwt.verify(req.headers.authorization, SECRET_KEY);
+        console.log(req.cookies);
+        console.log(SECRET_KEY);
+        console.log(jwt.verify(req.cookies.token, SECRET_KEY));
         req.decoded = jwt.verify(req.cookies.token, SECRET_KEY);
         return next();
     }
@@ -30,7 +33,7 @@ exports.auth = (req, res, next) => {
         }
         // 토큰의 비밀키가 일치하지 않는 경우
         if (error.name === 'JsonWebTokenError') {
-            console.log(error)
+            //console.log(error)
             return res.status(401).json({
                 code: 401,
                 message: '유효하지 않은 토큰입니다.'
@@ -62,7 +65,7 @@ exports.authManage = (req, res, next) => {
         }
         // 토큰의 비밀키가 일치하지 않는 경우
         if (error.name === 'JsonWebTokenError') {
-            console.log(error)
+           // console.log(error)
             return res.status(401).json({
                 code: 401,
                 message: '유효하지 않은 토큰입니다.'
