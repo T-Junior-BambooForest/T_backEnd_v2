1cbe6af708c5c3bef86fcdb41bd7579eb3ac68d5
chaewoo1002 <chaewoo1002@gmail.com>
2022-11-25 08:13:08 +0900

    FEAT : Instagram image upload 구현

--- routes/ins_upload.js
+++ routes/ins_upload.js
@@ -2,16 +2,21 @@ const axios = require('axios');
 require('dotenv').config();
 const models = require('../database/models');
 
+const mediaUrl = process.env.MEDIA_URL;
+const publishUrl = process.env.PUBLISH_URL;
+const logoImage = process.env.LOGO_IMAGE;
+const token = process.env.INS_TOKEN;
+
+let caption = "Test";
+let mdurl = null;
+let pburl = null;
+let userImageUrl = null;
+let logoContainer = null;
+let userImageContainer = null;
+let lastPublishContainer = null;
+
 exports.uploadInsta = async (data) => {
     try {
-        const mediaUrl = process.env.MEDIA_URL;
-        const publishUrl = process.env.PUBLISH_URL;
-        const logoImage = process.env.LOGO_IMAGE;
-        const token = process.env.INS_TOKEN;
-        let caption = "Test"
-        let mediaId = null;
-        let mdurl = mediaUrl + "?image_url=" + logoImage + "&caption=" + caption + "&access_token=" + token;
-        let pburl = `${publishUrl}?creation_id=${null}&access_token=${token}`;
         models.Board.findOne({
             include: [
                 {
@@ -29,31 +34,55 @@ exports.uploadInsta = async (data) => {
             result = JSON.parse(JSON.stringify(result));
             console.log(result);
             caption = `부산소마고 대나무숲 ${result.AllowBoard.AllowBoardCode}번째 제보\n${result.contents}\n- ${result.User.name}님 제보 -`;
-        }).then(() => {
-            mdurl = mediaUrl + "?image_url=" + logoImage + "&caption=" + encodeURI(caption) + "&access_token=" + token;
-            axios.post(mdurl)
-                .then((res) => {
-                    console.log(res.data.id);
-                    pburl = `${publishUrl}?creation_id=${res.data.id}&access_token=${token}`;
-                })
-                .catch((err) => {
-                    console.error(err);
-                }).then(() => {
-
-
-                axios.post(pburl)
-                    .then(() => console.log(`success`))
-                    .catch((err) => {
-                        console.error('error');
-                    })
-            })
+            result.Image == null ? notImage() : isImage(result);
         }).catch((err) => {
             console.error(err);
         })
-
     } catch (err) {
         console.log(err);
     }
+
+}
+
+async function notImage() {
+    mdurl = mediaUrl + "?image_url=" + logoImage + "&caption=" + caption + "&access_token=" + token;
+    axios.post(mdurl)
+        .then((res) => {
+            console.log(res.data.id);
+            pburl = publishUrl + "?creation_id=" + res.data.id + "&access_token=" + token;
+        }).then(() => {
+        publish(pburl)
+        }).catch((err) => {
+            console.error(err);
+        })
 }
 
 
+async function isImage(result){
+    mdurl = mediaUrl + "?image_url=" + logoImage + "&is_carousel_item=" + "true" + "&access_token=" + token;
+    userImageUrl = mediaUrl + "?image_url=" + null + "&is_carousel_item=" + "true" + "&access_token=" + token;
+    axios.post(mdurl)
+        .then((res) => {
+            logoContainer = res.data.id;
+        }).then(() => {
+            axios.post(userImageUrl)
+                .then((res) => {
+                    userImageContainer = res.data.id;
+                }).then(() => {
+                    lastPublishContainer = mediaUrl + "?children=" + logoContainer + "," + userImageContainer + "&media_type=" + "CAROUSEL" +  "&caption=" + caption + "&access_token=" + token;
+                    axios.post(lastPublishContainer);
+            }).then(() => {
+                publish(lastPublishContainer)
+            }).catch((err) => {
+                console.log(err);
+            })
+    })
+}
+
+async function publish(pburl) {
+    axios.post(pburl)
+        .then(() => console.log(`success`))
+        .catch((err) => {
+            console.error(err);
+        })
+}
