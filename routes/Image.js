ae98671bd7b4cb0867288a51109c7565bd9c02af
qlido <jason7530@naver.com>
2022-11-25 09:56:33 +0900

    FIX: 이미지업로드

--- routes/Image.js
+++ routes/Image.js
@@ -14,7 +14,7 @@ router.get('/:boardcode', (req, res) => {
        // console.log(fs.statSync(path.join(__dirname, '../Image', boardcode + ".jpg")).isFile());
     try {
          png = fs.statSync(path.join(__dirname, '../Image', boardcode + ".png")).isFile() ? fs.readFileSync(path.join(__dirname, '../Image', boardcode + ".png")).toString() : null;
-         png = new Buffer(png, 'base64').toString('ascii');
+         png = new Buffer(png, 'base64').toString('utf-8');
          console.log((png));
         return res.status(200).send('<img src="data:image/png;base64,'+(png)+'" />');
     } catch (error) {
@@ -22,7 +22,7 @@ router.get('/:boardcode', (req, res) => {
     }
     try {
          jpg = fs.statSync(path.join(__dirname, '../Image', boardcode + ".jpg")).isFile() ? fs.readFileSync(path.join(__dirname, '../Image', boardcode + ".jpg")).toString() : null;
-        jpg = new Buffer(jpg, 'base64').toString('ascii');
+        jpg = new Buffer(jpg, 'base64').toString('utf-8');
         console.log((jpg));
         return res.status(200).send('<img src="data:image/jpg;base64,'+(jpg)+'" />');
     } catch (error) {
@@ -31,7 +31,7 @@ router.get('/:boardcode', (req, res) => {
     }
     try {
          jpeg = fs.statSync(path.join(__dirname, '../Image', boardcode + ".jpeg")).isFile() ? fs.readFileSync(path.join(__dirname, '../Image', boardcode + ".jpeg")).toString() : null;
-        jpeg = new Buffer(jpeg, 'base64').toString('ascii');
+        jpeg = new Buffer(jpeg, 'base64').toString('utf-8');
         console.log((jpeg));
         return res.status(200).send('<img src="data:image/jpeg;base64,'+(jpeg)+'" />');
     } catch (error) {
