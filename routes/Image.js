3933e12893f9c84c109197316bdc8781b4cb4f07
qlido <jason7530@naver.com>
2022-11-25 09:51:29 +0900

    FIX: 이미지업로드

--- routes/Image.js
+++ routes/Image.js
@@ -10,27 +10,30 @@ const SECRET_KEY = 'process.env.JWT_SECRET';
 router.get('/:boardcode', (req, res) => {
         const boardcode = req.params.boardcode;
 
-        let jpg,png,jpeg;
+        let jpg,jpgpng,jpeg;
        // console.log(fs.statSync(path.join(__dirname, '../Image', boardcode + ".jpg")).isFile());
     try {
          png = fs.statSync(path.join(__dirname, '../Image', boardcode + ".png")).isFile() ? fs.readFileSync(path.join(__dirname, '../Image', boardcode + ".png")).toString() : null;
-         console.log(atob(png));
-        return res.status(200).send('<img src="data:image/png;base64,'+atob(png)+'" />');
+         png = new Buffer(png, 'base64').toString('ascii');
+         console.log((png));
+        return res.status(200).send('<img src="data:image/png;base64,'+(png)+'" />');
     } catch (error) {
          png = null;
     }
     try {
          jpg = fs.statSync(path.join(__dirname, '../Image', boardcode + ".jpg")).isFile() ? fs.readFileSync(path.join(__dirname, '../Image', boardcode + ".jpg")).toString() : null;
-        console.log(atob(jpg));
-        return res.status(200).send('<img src="data:image/jpg;base64,'+atob(jpg)+'" />');
+        jpg = new Buffer(jpg, 'base64').toString('ascii');
+        console.log((jpg));
+        return res.status(200).send('<img src="data:image/jpg;base64,'+(jpg)+'" />');
     } catch (error) {
          jpg = null;
 
     }
     try {
          jpeg = fs.statSync(path.join(__dirname, '../Image', boardcode + ".jpeg")).isFile() ? fs.readFileSync(path.join(__dirname, '../Image', boardcode + ".jpeg")).toString() : null;
-        console.log(atob(jpeg));
-        return res.status(200).send('<img src="data:image/jpeg;base64,'+atob(jpeg)+'" />');
+        jpeg = new Buffer(jpeg, 'base64').toString('ascii');
+        console.log((jpeg));
+        return res.status(200).send('<img src="data:image/jpeg;base64,'+(jpeg)+'" />');
     } catch (error) {
          jpeg = null;
     }
