d0659ac8e53bd59cded84880d073f4bd19b2290a
qlido <jason7530@naver.com>
2022-11-25 09:11:43 +0900

    FIX: 이미지업로드

--- routes/Image.js
+++ routes/Image.js
@@ -13,26 +13,27 @@ router.get('/:boardcode', (req, res) => {
        // console.log(fs.statSync(path.join(__dirname, '../Image', boardcode + ".jpg")).isFile());
     try {
          png = fs.statSync(path.join(__dirname, '../Image', boardcode + ".png")).isFile() ? fs.readFileSync(path.join(__dirname, '../Image', boardcode + ".png")).toString() : null;
+        return res.status(200).send('<img src="data:image/png;base64,'+png+'" />');
     } catch (error) {
          png = null;
     }
     try {
          jpg = fs.statSync(path.join(__dirname, '../Image', boardcode + ".jpg")).isFile() ? fs.readFileSync(path.join(__dirname, '../Image', boardcode + ".jpg")).toString() : null;
+        return res.status(200).send('<img src="data:image/jpg;base64,'+jpg+'" />');
     } catch (error) {
          jpg = null;
+
     }
     try {
          jpeg = fs.statSync(path.join(__dirname, '../Image', boardcode + ".jpeg")).isFile() ? fs.readFileSync(path.join(__dirname, '../Image', boardcode + ".jpeg")).toString() : null;
+        return res.status(200).send('<img src="data:image/jpeg;base64,'+jpeg+'" />');
     } catch (error) {
          jpeg = null;
     }
 
     try{
-        let Image = png ?? jpg ?? jpeg
-        if (Image == undefined || Image == null) {
-            return res.status(404).send("Not Found");
-        } else
-            res.status(200).send(`<img src={${Image}} />`);
+
+
     } catch (e) {
         console.log(e);
         return res.status(500).send("Server Error");
