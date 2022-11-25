aed9ee80e9a0e847070655eee045491d5cd253c0
qlido <jason7530@naver.com>
2022-11-25 13:45:27 +0900

    ADD: 매니저 관리

--- routes/whoareyou.js
+++ routes/whoareyou.js
@@ -5,6 +5,7 @@ require("dotenv").config({path: __dirname + '/.env'});
 const models = require('../database/models');
 const { auth } = require('./isLogined');
 const SECRET_KEY = 'process.env.JWT_SECRET';
+let Manager = [66,45,43]
 router.get('/', auth, (req, res) => {
     const code = req.decoded.code;
     models.Users.findOne({
@@ -12,6 +13,8 @@ router.get('/', auth, (req, res) => {
             code: code
         }
     }).then((result) => {
+        result = JSON.parse(JSON.stringify(result));
+        if(Manager.includes(result.code)) result.isManager = true;
         return res.status(200).json({
             code: 200,
             message: '토큰은 정상입니다.',
